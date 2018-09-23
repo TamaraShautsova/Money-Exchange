bc7e8ba76e0a25d25a0b477852652c7823855665
tamara shautsova <t.shautsova@gmail.com>
2018-09-23 16:31:32 +0300

    money task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var exchange = {};
+    var halfDollar = 50;
+    var quarter = 25;
+    var dimes = 10;
+    var nickel = 5;
+    var penni = 1;
+
+    if (currency >= 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    while (currency > 0) {
+        if(currency - halfDollar >= 0) {
+            currency -= halfDollar;
+            if (exchange['H']){
+                exchange['H']++;
+            }
+            else exchange['H'] = penni;
+            continue;
+        }
+
+        if(currency - quarter >= 0) {
+            currency -= quarter;
+            if (exchange['Q']){
+                exchange['Q']++;
+            }
+            else exchange['Q'] = penni;
+            continue;
+        }
+
+        if(currency - dimes >= 0) {
+            currency -= dimes;
+            if (exchange['D']){
+                exchange['D']++;
+            }
+            else exchange['D'] = penni;
+            continue;
+        }
+
+        if(currency - nickel >= 0) {
+            currency -= nickel;
+            if (exchange['N']){
+                exchange['N']++;
+            }
+            else exchange['N'] = penni;
+            continue;
+        }else{
+            exchange['P'] = currency;
+            break;
+        }
+
+    }
+
+  return exchange;
 }
